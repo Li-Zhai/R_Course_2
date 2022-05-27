845c4fbc743b5932b0c0aafe0be2bef2b42b8c23
Li-Zhai <2110866551@qq.com>
2022-05-27 11:39:27 +0800

    After completing week 1.

--- Week_1.R
+++ Week_1.R
@@ -23,4 +23,39 @@ dim(x) = c(2, 5)  # create matrix 2
 cbind(1:3, 10:12)
 rbind(1:3, 10:12)  # create matrix 3
 
-x = 
+x = factor(c("yes", "yes", "no", "yes", "no"))
+table(x)  # get {no: 2, yes: 3}
+unclass(x)  # reveals the embedded vector
+# determines the order of levels manually
+x = factor(c("yes", "yes", "no", "yes", "no"), levels = c("yes", "no"))
+
+is.na(x)  # sees if x is NA
+is.nan(x)  # sees if x is NAN
+# NAN is also NA; NA is not always NAN
+x = c(1, NaN, NA, 10, 3)
+is.na(x)  # the second and the third is TRUE
+is.nan(x)  # only the second is TRUE
+
+x = data.frame(foo = 1:4, bar = c(T, T, F, F))  # create dataframe
+
+x = 1:3
+names(x) = c("foo", "bar", "norf")  # add names to values
+x = list(a = 1, b = 2, c = 3)  # a/b/c are names
+x = matrix(1:4, nrow = 2, ncol = 2)
+dimnames(x) = list(c("a", "b"), c("c", "d"))  # names for rows and cols
+
+# Quiz
+x = read.csv("hw1_data.csv")
+colnames(x)  # columns names
+x[1:2,]
+x[152:153,]
+x[47,"Ozone"]
+sum(is.na(x["Ozone"]))
+mean(x$Ozone, na.rm = TRUE)
+class(x$Ozone)  # integer
+class(x[["Ozone"]])  # integer
+class(x["Ozone"])  # data.frame
+y = x[x$Ozone>31 & x$Temp>90,]
+mean(y$Solar.R, na.rm = TRUE)
+mean(x[x$Month==6,]$Temp, na.rm = TRUE)
+max(x[x$Month==5,]$Ozone, na.rm = TRUE)
